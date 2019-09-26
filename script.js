74344ae996bfde00631da4c0b3f9360f72216ba6
unknown <sathyanasa@gmail.com>
2019-09-26 13:09:35 -0700

    Added website's first files!

---
+++ script.js
@@ -0,0 +1,26 @@
+
+$(window).scroll(function(){
+  $(".top").css("opacity", 1 - $(window).scrollTop() / 100);
+});
+
+$(window).scroll(function() {
+// 100 = The point you would like to fade the nav in.
+
+	if ($(window).scrollTop() > 100 ){
+
+ 		$('.navlist').addClass('show');
+
+  } else {
+
+    $('.navlist').removeClass('show');
+
+ 	};
+});
+
+$('.scroll').on('click', function(e){
+		e.preventDefault()
+
+  $('html, body').animate({
+      scrollTop : $(this.hash).offset().top
+    }, 1500);
+});
